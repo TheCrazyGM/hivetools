40b507b005f9de446d5a1dcb0a70f0b730e1b37e
Michael Garcia <thecrazygm@gmail.com>
2020-04-16 10:54:45 -0500

    cleanup on lost bee

--- hivetools/lost_bee.py
+++ hivetools/lost_bee.py
@@ -4,7 +4,6 @@ from getpass import getpass
 from beemgraphenebase.account import PasswordKey
 from tabulate import tabulate
 
-hive_id = "thecrazygm"
 hive_id = input("Hive User ID: ")
 brain_key = getpass(prompt='Master Password: ')
 
@@ -13,9 +12,7 @@ data = []
 for role in roles:
     keys = PasswordKey(hive_id, brain_key, role=role, prefix="STM")
     priv_key = keys.get_private()
-#    priv = keys.get_private()
     pub_key = keys.get_public()
-#   pub = keys.get_public()
-    data += [[role, str(pub_key), str(priv_key)]]
+    data.append([role, str(pub_key), str(priv_key)])
 
 print(tabulate(data, headers=["Role", "Public Key", "Private Key"]))
