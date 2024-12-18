683a56b92921b27b05c29c966984e91b1ce38bf0
Michael Garcia <thecrazygm@gmail.com>
2024-12-18 04:44:14 -0500

    fixed tx_all

--- hivetools/tx_all.py
+++ hivetools/tx_all.py
@@ -15,21 +15,21 @@ from beem import hive
 from beem.wallet import Wallet
 from dotenv import load_dotenv
 from hiveengine.tokens import Tokens
-from hiveengine.wallet import Wallet as seWallet
+from hiveengine.wallet import Wallet as heWallet
 
 
-def wasp():
+def dump_all():
     load_dotenv()
     """Go down the list of hive Engine tokens and transfer full balance to destination"""
     ACTIVE_WIF = os.getenv("ACTIVE_WIF")
     send_to = input("Enter destination: ")
 
-    hv = hive(keys=[ACTIVE_WIF], nodes="https://api.hiveit.com")
-    w = Wallet(hive_instance=hv)
+    hv = hive(keys=[ACTIVE_WIF], nodes="https://api.hive.blog")
+    w = Wallet(blockchain_instance=hv)
     t = Tokens()
     usr = w.getAccountFromPrivateKey(ACTIVE_WIF)
-    sew = seWallet(account=usr, hive_instance=hv)
-    tokens = sew.get_balances()
+    hew = heWallet(account=usr, blockchain_instance=hv)
+    tokens = hew.get_balances()
     for token in tokens:
         symbol = token["symbol"]
         info = t.get_token(symbol)
@@ -38,10 +38,10 @@ def wasp():
         balance = float(f"{b:.{p}f}")
         if balance > 0:
             print(f"[ Transfering {balance} of {symbol} to {send_to} ]")
-            sew.transfer(send_to, balance, symbol, memo="Automatic transfer")
+            hew.transfer(send_to, balance, symbol, memo="Automatic transfer")
             time.sleep(1)
     return None
 
 
 if __name__ == "__main__":
-    wasp()
+    dump_all()
