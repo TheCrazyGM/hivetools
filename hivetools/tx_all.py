f0cf49bbe048ac1c7c192c2a0e39e8080891dcdb
Michael Garcia <thecrazygm@gmail.com>
2024-12-18 08:42:31 -0500

    clean up dumpall

--- hivetools/tx_all.py
+++ hivetools/tx_all.py
@@ -20,28 +20,24 @@ from hiveengine.wallet import Wallet as heWallet
 
 def dump_all():
     load_dotenv()
-    """Go down the list of hive Engine tokens and transfer full balance to destination"""
     ACTIVE_WIF = os.getenv("ACTIVE_WIF")
     send_to = input("Enter destination: ")
 
     hv = Hive(keys=[ACTIVE_WIF], nodes="https://api.hive.blog")
     w = Wallet(blockchain_instance=hv)
-    print("[Getting all tokens]")
-    t = Tokens()
     usr = w.getAccountFromPrivateKey(ACTIVE_WIF)
-    print(usr)
     hew = heWallet(account=usr, blockchain_instance=hv)
     tokens = hew.get_balances()
+
     for token in tokens:
         symbol = token["symbol"]
-        info = t.get_token(symbol)
-        p = info["precision"]
-        b = float(token["balance"])
-        balance = float(f"{b:.{p}f}")
+        info = Tokens().get_token(symbol)
+        balance = float(f"{float(token['balance']):.{info['precision']}f}")
         if balance > 0:
             print(f"[ Transfering {balance} of {symbol} to {send_to} ]")
             hew.transfer(send_to, balance, symbol, memo="Automatic transfer")
             time.sleep(1)
+
     return None
 
 
